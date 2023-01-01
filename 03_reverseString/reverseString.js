b6b7ac46c4a5d9118d03eeb1309b498119e7ebcc
ricky-yosh <63967513+ricky-yosh@users.noreply.github.com>
2023-01-01 11:22:29 -0800

    Add 03 js practice

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(stringInput) {
+    let reversedString = "";
+    for(let index = stringInput.length - 1; index >= 0; index--) {
+        let currentLetter = stringInput[index];
+        reversedString = reversedString + currentLetter;
+    }
+    return reversedString;
 };
+// Testing Purposes
+// let answer = reverseString("apple");
 
 // Do not edit below this line
 module.exports = reverseString;
