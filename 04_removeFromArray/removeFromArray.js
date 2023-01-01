a356ff2bc8023699cafbc6fa009118283936286b
ricky-yosh <63967513+ricky-yosh@users.noreply.github.com>
2023-01-01 11:43:08 -0800

    Add 04 js practice

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arrayInput, ...elementsToRemove) {
+    let currentArray = [];
+    const removableElementsArray = Array.from(elementsToRemove);
+    for (const inputElement of arrayInput)
+    {
+        if(!elementsToRemove.includes(inputElement))
+        {
+            currentArray.push(inputElement);
+        }
+    }
+    const finalArray = currentArray;
+    return finalArray
 };
 
 // Do not edit below this line
