bdfa233d8dd4b845e5942cf7ad5cfe3688478467
ricky-yosh <63967513+ricky-yosh@users.noreply.github.com>
2022-12-31 23:49:30 -0800

    Add 02 js practice

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(stringInput, repeatCount) {
+    let finalString = "";
+    if (repeatCount < 0) {
+        finalString = "ERROR";
+    }
+    else {
+        for (let count = 0; count < repeatCount; count++) {
+            finalString = finalString + stringInput;
+        }
+    }
+    return finalString;
 };
 
 // Do not edit below this line
