7d0c3cb2ae98837df15a5f8fe40219920cebc4f9
ricky-yosh <63967513+ricky-yosh@users.noreply.github.com>
2023-01-01 18:47:12 -0800

    Add 06 js practice

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,16 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    let leapYearBoolean = false;
+    const divisibleBy4 = year % 4 == 0;
+    const divisibleBy100 = year % 100 == 0;
+    const divisibleBy400 = year % 400 == 0;
+    if (divisibleBy4 && !divisibleBy100 || divisibleBy400)
+    {
+        leapYearBoolean = true;
+    }
+    return leapYearBoolean;
 };
+
+leapYears(1996)
 
 // Do not edit below this line
 module.exports = leapYears;
