e6b445370591b7d614b499645b3fa4f6de5d626d
ricky-yosh <63967513+ricky-yosh@users.noreply.github.com>
2023-01-01 18:36:42 -0800

    Add 05 js practice

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
+const sumAll = function(startingNum, endingNum) {
+    if(!(Number.isInteger(startingNum) && Number.isInteger(endingNum) && startingNum >= 0 && endingNum >= 0))
+    {
+        return "ERROR";
+    }
 
+    const largerNum = Math.max(startingNum, endingNum);
+    const smallerNum = Math.min(startingNum, endingNum);
+
+ 
+    let currentSum = 0;
+    for (let count = smallerNum; count <= largerNum; count++)
+    {
+        currentSum += count;
+    }
+    const finalSum = currentSum;
+    return finalSum;
 };
+
+sumAll(-10, 4)
 
 // Do not edit below this line
 module.exports = sumAll;
