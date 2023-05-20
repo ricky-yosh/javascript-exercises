85ea3f66204ecad0e54e4787b9f54b4db94090aa
ricky-yosh <63967513+ricky-yosh@users.noreply.github.com>
2023-05-20 11:04:52 -0700

    Create Palindrome function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,18 @@
-const palindromes = function () {
-
+const palindromes = function (stringInput) {
+    let changedString = stringInput.replace(/[^a-zA-Z]/g, '').toLowerCase();
+    let stringIsFull = stringInput.length;
+    while(stringIsFull) {
+        firstLetterIsLast = stringInput[0] === stringInput[stringInput.length - 1];
+        if (!firstLetterIsLast) {
+            return false;
+        }
+        stringInput.substring(1, stringInput.length - 1);
+        stringIsFull = stringIsFull.length;
+    }
+    return true;
 };
+
+palindromes("applE!");
 
 // Do not edit below this line
 module.exports = palindromes;
