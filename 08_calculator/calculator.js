941bb1cdcefc3dbea56f6641cc0779f2e1c7dc12
ricky-yosh <63967513+ricky-yosh@users.noreply.github.com>
2023-05-20 10:12:16 -0700

    Create Calculator function

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(a, b) {
+  const sum = a + b;
+	return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	const difference = a - b;
+  return difference;
 };
 
-const sum = function() {
-	
+const sum = function(arrOfNums) {
+  const totalSum = arrOfNums.reduce((a, b) => a + b, 0);
+  return totalSum;
 };
 
-const multiply = function() {
-
+const multiply = function(arrOfNums) {
+  const totalProduct = arrOfNums.reduce((a, b) => a * b);
+  return totalProduct;
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+  let exponentiation = base;
+  for (let multiplier = 1; multiplier < exponent; multiplier++) {
+    exponentiation *= base;
+  }
+  return exponentiation;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let factorialValue = 1;
+  while(num > 0) {
+    factorialValue *= num;
+    num--;
+  }
+  return factorialValue;
 };
 
 // Do not edit below this line
