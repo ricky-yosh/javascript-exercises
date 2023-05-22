81ca10d59e19b03ca8e9b5ddecdebaaa13b53239
ricky-yosh <63967513+ricky-yosh@users.noreply.github.com>
2023-05-22 14:30:58 -0700

    Create findTheOldest function

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const ages = people.reduce((firstPerson, secondPerson) => {
+        const firstAge = (firstPerson.yearOfDeath != null ? firstPerson.yearOfDeath: 2023) - firstPerson.yearOfBirth;
+        const secondAge = (secondPerson.yearOfDeath != null ? secondPerson.yearOfDeath: 2023) - secondPerson.yearOfBirth;
+        return firstAge > secondAge ? firstPerson : secondPerson;
+    });
+    const returnObject = ages;
+    return returnObject;
 };
 
 // Do not edit below this line
