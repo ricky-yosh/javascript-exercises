01a1b0e22973dd1b8906aaf3f9c86f92b4a077f1
ricky-yosh <63967513+ricky-yosh@users.noreply.github.com>
2023-05-22 09:44:42 -0700

    Create fibonacci function

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(fibIndex) {
+    let finalNum = 1;
+    let prevNum = 0;
+    let temp;
+    let inputZeroOrMore = fibIndex >= 0;
+    let counter = 1;
 
+    if (inputZeroOrMore) {
+        while (counter < fibIndex) {
+            temp = finalNum;
+            finalNum += prevNum;
+            prevNum = temp;
+            counter++;
+        }
+    } else {
+        finalNum = "OOPS";
+    }
+    return finalNum;
 };
+
+fibonacci(3);
 
 // Do not edit below this line
 module.exports = fibonacci;
